3cbb2dd647e025dcc9625d99dce3f42a4e0e8568
songzongyao <704140897@qq.com>
2019-03-01 16:07:32 +0800

    20190301

---
+++ src/DataStructure/StackTest.java
@@ -0,0 +1,13 @@
+package DataStructure;
+import java.util.Stack;
+/**
+ * @Author 宋宗垚
+ * @Date 2019/2/25 20:24
+ * @Description 用于测试栈的使用
+ */
+public class StackTest {
+    public static void main(String args[]){
+        Stack<String> stack;
+    }
+
+}
